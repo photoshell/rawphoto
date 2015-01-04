97c79243881a87e5be2c22215668294307cfd41b
Sam Whited <sam@samwhited.com>
2015-01-04 16:44:35 -0500

    Update setup.py and add manifest

--- setup.py
+++ setup.py
@@ -8,4 +8,26 @@ setup(
     author_email='sam@samwhited.com',
     url='https://github.com/photoshell/rawphoto',
     packages=['rawphoto'],
+    keywords=['encoding', 'images', 'cr2'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Environment :: Other Environment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description="""\
+Raw image file parser
+---------------------
+
+Currently supports
+  - Canon CR2
+
+This version has only been tested with Python 3.4.
+"""
+
 )
