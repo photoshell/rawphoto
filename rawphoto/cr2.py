005ec0f9099811997843e7fc946ad0d03e2bef68
Sam Whited <sam@samwhited.com>
2015-01-10 12:26:09 -0500

    Fix bug when loading byte array
    Add more tests

--- rawphoto/cr2.py
+++ rawphoto/cr2.py
@@ -1,7 +1,7 @@
-import struct
-
 from collections import namedtuple
 from io import BytesIO
+
+import struct
 
 # Mapping from manufacturer to associated endianness as accepted by struct
 endian_flags = {
@@ -15,7 +15,7 @@ _HeaderFields = namedtuple("HeaderFields", [
 ])
 
 _IfdEntryFields = namedtuple("IfdEntryFields", [
-    "tag_id", "tag_name", "tag_type", "value_len", "raw_value"
+    "tag_id", "tag_name", "tag_type", "tag_type_key", "value_len", "raw_value"
 ])
 
 subdirs = [0x8769, 0x927c]
@@ -94,7 +94,7 @@ tag_types = {
     0x4: 'L',  # Unsigned long
     0x5: 'L',  # Unsigned rational (ignoring second half)
     0x6: 'b',  # Signed char
-    0x7: 'p',  # Byte sequence
+    0x7: 's',  # Byte sequence
     0x8: 'h',  # Signed short
     0x9: 'l',  # Signed long
     0xA: 'l',  # Signed rational (ignoring second half)
@@ -145,7 +145,7 @@ class IfdEntry(_IfdEntryFields):
         else:
             tag_name = tag_id
         tag_type = tag_types[tag_type_key]
-        if struct.calcsize(tag_type) > 4 or tag_type == 's' or tag_type == 'p':
+        if struct.calcsize(tag_type) > 4 or tag_type == 's':
             # If the value is a pointer to something small:
             [raw_value] = unpack_at('L')
         else:
@@ -158,7 +158,7 @@ class IfdEntry(_IfdEntryFields):
             fhandle.seek(pos)
 
         return super(IfdEntry, cls).__new__(cls, tag_id, tag_name, tag_type,
-                                            value_len, raw_value)
+                                            tag_type_key, value_len, raw_value)
 
 
 class Ifd(object):
@@ -199,15 +199,16 @@ class Ifd(object):
     def get_value(self, entry):
         tag_type = entry.tag_type
         size = struct.calcsize(tag_type)
-        if size > 4 or tag_type == 's' or tag_type == 'p':
+        if size > 4 or tag_type == 's':
             # Read value
             pos = self.fhandle.seek(0, 1)
             self.fhandle.seek(entry.raw_value)
-            if tag_type == 's' or tag_type == 'p':
+            if tag_type == 's':
                 buf = self.fhandle.read(entry.value_len)
-                [value] = struct.unpack_from('{}{}'.format(entry.value_len,
-                                                           tag_type), buf)
-                if tag_type == 's':
+                [value] = struct.unpack('{}{}'.format(entry.value_len,
+                                                      tag_type), buf)
+                # If this is a null terminated string
+                if entry.tag_type_key == 0x02:
                     value = value.rstrip(b'\0').decode("utf-8")
             else:
                 buf = self.fhandle.read(size)
--- tests/ifd_test.py
+++ tests/ifd_test.py
@@ -6,9 +6,22 @@ import pytest
 ifd_bytes = b'''\
 \x02\x00\x01\x02\x04\x00\x01\x00\x00\x00\x08\xd7\x00\x00\x02\x02\x04\x00\x01\x00\x00\x00P-\x00\x00\xee\xb5\x00\x00
 '''
+
 ifd_bytes_sub_ifd = b'''\
 \x01\x00i\x87\x04\x00\x01\x00\x00\x00\x12\x00\x00\x00\x12\x00\x00\x00\
 \x01\x00\x01\x02\x04\x00\x01\x00\x00\x00\x08\xd7\x00\x00\x00\x00\x00\x00
+'''
+
+ifd_bytes_string_value = b'''\
+\x01\x00\
+\x0f\x01\x02\x00\x06\x00\x00\x00\x12\x00\x00\x00\
+\x00\x00\x00\x00Canon\x00
+'''
+
+ifd_bytes_byte_array = b'''\
+\x01\x00\
+\x0f\x01\x07\x00\x06\x00\x00\x00\x12\x00\x00\x00\
+\x00\x00\x00\x00Canon\x00
 '''
 
 
@@ -54,6 +67,19 @@ def test_get_value_invalid_offset():
     assert ifd.get_value(ifd.entries['thumbnail_length']) == \
         ifd.entries['thumbnail_length'].raw_value
 
-# TODO: Add test of reading string and byte array
+
+def test_ifd_get_string_value():
+    ifd = Ifd("<", blob=ifd_bytes_string_value)
+    val = ifd.get_value(ifd.entries['make'])
+    assert isinstance(val, (type(u""), str))
+    assert val == 'Canon'
+
+
+def test_ifd_get_byte_array_value():
+    ifd = Ifd("<", blob=ifd_bytes_byte_array)
+    val = ifd.get_value(ifd.entries['make'])
+    assert isinstance(val, bytes)
+    assert val == b'Canon\x00'
+
 # TODO: Add test of reading decimal or other value with indirect
 # TODO: Add test of reading in-place value
